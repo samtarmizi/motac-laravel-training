526196693886a88493d42f8f4662fc653aa618e1
TARMIZI SANUSI <realtarmizisanusi@gmail.com>
2024-05-14 12:30:35 +0800

    add bs table

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,5 +1,32 @@
-<h1>This is tasks index</h1>
+@extends('layouts.app')
 
-@foreach($tasks as $task)
-    <p>{{ $task->title }} - {{ $task->description }}</p>
-@endforeach
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Index Tasks') }}</div>
+
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Task Title</th>
+                                <th>Task Description</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($tasks as $task)
+                                <tr>
+                                    <td>{{ $task->title }}</td>
+                                    <td>{{ $task->description }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
